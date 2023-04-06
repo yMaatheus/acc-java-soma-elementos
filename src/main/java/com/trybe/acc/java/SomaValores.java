ac10efebf778047dd45753764fbf38d39439ce94
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-05 22:19:42 -0300

    feat: input sum first  element of array with last element of array

--- src/main/java/com/trybe/acc/java/SomaValores.java
+++ src/main/java/com/trybe/acc/java/SomaValores.java
@@ -3,26 +3,28 @@ package com.trybe.acc.java;
 import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * Class SomaValores.
+ */
 public class SomaValores {
-	
+
+  /**
+   * Initial Method.
+   */
   public static void main(String[] args) {
-    // Ler a entrada de um número pelo console
     Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-    int[] myArray = new int [3]; 
+    int[] myArray = new int[3];
 
-    // Laço de repetição 
     for (int index = 0; index < 3; index++) {
       System.out.println("Digite um número:");
       myArray[index] = scan.nextInt();
     }
 
-    // Seu código aqui
+    int soma = myArray[0] + myArray[2];
 
-
-    // Imprime a saída no console. OBS: descomente a próxima linha
-    //System.out.println("A soma é: " + soma);
-
+    System.out.println("A soma é: " + soma);
+    scan.close();
   }
 
 }
